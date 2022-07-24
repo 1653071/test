42e7b12292788e337b62e5600484c7c3546986da
Quang Nguyen Minh <minhquang9803vnnn@gmail.com>
2022-07-25 02:32:51 +0700

    2

--- interface/repository.interface.ts
+++ interface/repository.interface.ts
@@ -8,11 +8,19 @@ export class Repository<T extends Document>{
 	async post () : Promise<any>{
 		return await this.a.find({})
 	}
-	async postData () : Promise<any>{
+	async postDataField (obj: JSON) : Promise<any>{
 		    var myquery = { _id:new mongoose.Types.ObjectId("62dd7a9fbf7b0ad079b6878d")};
-			var newvalues = { $set: {name: "Mickey11111" } };
+			var newvalues = { $set: obj };
 			return await this.a.updateOne(myquery, newvalues)
 		
+	}
+	async postData(obj: JSON):Promise<any>{
+		await this.a.findOne({_id:new mongoose.Types.ObjectId("62dd7a9fbf7b0ad079b6878d")}).then(res=>{
+            
+		})
+		
+		var newvalues = { $set: obj };
+		return await this.a.updateOne(myquery, newvalues)
 	}
 
 }
